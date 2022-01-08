de5fa560f65e9b94ee95ec1ce6c2ce8e22ba35d1
Matthew Kaufmann <matthew.e.kaufmann@gmail.com>
2022-01-07 23:10:26 -0600

    Finished cleaning script for Netflix raw data

--- Netflix watch habits/analysis.py
+++ Netflix watch habits/analysis.py
@@ -1,29 +1,36 @@
 import pandas as pd
 
-def read_and_clean(filepath):
+def read_and_clean(filepath, profile=None):
     """
     Reads in and cleans a csv file to analyze for Netflix watch patterns
 
     Input:
         filepath (filepath): csv filepath
+        profile (str): profile name to filter
     
     Returns (pd.dataframe): cleaned dataframe
     """
     data = pd.read_csv(filepath)
+    # filter a user if there is one
+    if profile:
+        data = data[ (data["Profile Name"] == profile) ]
+
+    # remove videos with supplemental video types (these are hooks, recaps,
+    # teases, previews, etc.)
+
+    data = data[ (data["Supplemental Video Type"].isnull()) ]
+
     data_reduced = data.drop(['Attributes', 'Supplemental Video Type', 'Bookmark',
                             'Latest Bookmark','Country'], axis = 1)
     data_reduced["Start Time"] =  pd.to_datetime(data_reduced["Start Time"],
                                                 utc=True)
-
+    
     # change the Start Time column into the dataframe's index
     data_utc = data_reduced.set_index('Start Time')
     # convert from UTC timezone to central time
     data_utc.index = data_utc.index.tz_convert('US/Central')
     # reset the index so that Start Time becomes a column again
     data_cst = data_utc.reset_index()
-
-    #Reorder profile back to first
-    data_cst = data_cst[['Profile Name', 'Start Time', 'Duration', 'Title', 'Device Type']]
 
     # convert the duration column to a time delta
     data_cst["Duration"] = pd.to_timedelta(data_cst["Duration"])
@@ -32,45 +39,85 @@ def read_and_clean(filepath):
     # trailers etc (which Netflix counts as views)
     data_cst = data_cst[ (data_cst["Duration"] > '0 days 00:00:30') ]
 
+    # add day and hour of watch time
+    data_cst["Start Day"] = data_cst["Start Time"].dt.weekday
+    data_cst["Start Hour"] = data_cst["Start Time"].dt.hour
+
+    # reorder column order
+    data_cst = data_cst[['Profile Name', 'Start Time', 'Start Day', 'Start Hour', 'Duration', 'Title', 'Device Type']]
+
     # get show info more organized by splitting title column
     title_info = data_cst["Title"].str.split(":").apply(lambda row: [col.strip() for col in row])
-    col_titles = ["Simple Title", "Subtitle", "Season", "Episode"]
-    split_show_info = []
+    title, subtitle, season, episode = [], [], [], []
 
     for row in title_info:
-        show_data = []
-        show_data.append(row[0])
+        title.append(row[0])
         if len(row) == 2:
-            show_data.append(row[1])
-            show_data.append("")
-            show_data.append("")
-            split_show_info.append(show_data)
+            subtitle.append(row[1])
+            season.append("")
+            episode.append("")
             continue
-        if len(row) > 2:
-            if "Season" in row[1]:
-                show_data.append("")
-                show_data.append(row[1])
-                show_data.append(row[2])
-                split_show_info.append(show_data)
+        if "Part " in row[-1] and len(row) > 3: # for episodes such as Manchester: Part I
+                row = row[:]
+                new_item = row.pop(-2) + ": " + row.pop(-1)
+                row.append(new_item)
+        if len(row) > 2:    
+            # special cases
+            if "Book" in row[1]:
+                subtitle.append("")
+                if "Avatar" in row[0]:
+                    season.append(row[1])
+                    episode.append(row[2])
+                else:
+                    season.append(": ".join(row[1:3]))
+                    episode.append(row[3])
                 continue
-            else:
-                show_data.append(row[1])
-                show_data.append(row[2])
-                show_data.append(row[3])
-                split_show_info.append(show_data)
+            if "Bleach" in row[0]:
+                subtitle.append("")
+                season.append(row[1])
+                episode.append(row[2])
                 continue
-        show_data.append("")
-        show_data.append("")
-        show_data.append("")
-        split_show_info.append(show_data)
+            if "Comedians in Cars Getting Coffee" == row[0]:
+                subtitle.append("")
+                if row[1] == "New 2018":
+                    row[1] = row[1] + ": " + row.pop(2)
+                season.append(row[1])
+                episode.append(": ".join(row[2:]))
+                continue
+            if row[1] == "Black & White":
+                subtitle.append(row[1])
+                season.append(row[2])
+                episode.append(row[4])
+                continue
+            # general rules
+            if "Season" in row[1] or "Part" in row[1] or "Volume" in row[1]:
+                subtitle.append("")
+                season.append(row[1])
+                episode.append(row[2])
+                continue
+            elif len(row) == 3:
+                subtitle.append(row[1])
+                season.append("")
+                episode.append(row[2])
+                continue
+            elif len(row) == 4:
+                subtitle.append(row[1])
+                season.append(row[2])
+                episode.append(row[3])
+                continue
+                    # movie or some other thing of 1 item length
+        subtitle.append("")
+        season.append("")
+        episode.append("")
 
-    show_info = pd.DataFrame(split_show_info, columns=col_titles)
-
-    detailed_info = pd.concat([data_cst, show_info], axis=1)
-    cleaned_info = detailed_info.drop(["Title"], axis = 1)
-    final_df = cleaned_info[['Profile Name', 'Start Time', 'Duration', 'Device Type', 'Simple Title', 'Subtitle', 'Season', 'Episode']]
+    base_df = data_cst.drop(["Title"], axis = 1)
+    final_df = base_df.assign(Title = title,
+                                         Subtitle = subtitle,
+                                         Season = season,
+                                         Episode = episode)
+    
+    final_df.to_csv(r"C:\Users\matth\OneDrive\Documents\Coding Projects\Netflix watch habits\output.csv", index=False)
     return final_df
 
-print(read_and_clean("sample_data.csv"))
-        
-        
+to_anlyze = read_and_clean("ViewingActivity.csv", "Matthew")
+
